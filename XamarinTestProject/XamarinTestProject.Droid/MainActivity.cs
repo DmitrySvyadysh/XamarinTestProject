621f37292fbbd6d70ba13153d529dbec60c9ffa1
Dmitry Svyadysh <d.svyadysh@itransition.com>
2017-05-15 17:16:51 +0300

    Make android project compilable again

--- XamarinTestProject/XamarinTestProject.Droid/MainActivity.cs
+++ XamarinTestProject/XamarinTestProject.Droid/MainActivity.cs
@@ -21,8 +21,6 @@ namespace XamarinTestProject.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
-
-            NonexistentMethod();
         }
     }
 }
