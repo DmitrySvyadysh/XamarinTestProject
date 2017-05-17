4e803d8c6cb35c22697af5cc23899c949bb0c4be
Dmitry Svyadysh <d.svyadysh@itransition.com>
2017-05-17 15:29:37 +0300

    Added test project as console app

---
+++ XamarinTestProject.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using NUnitLite;
+
+namespace xamarinTestProject.TestConsole
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new AutoRun().Execute(args);
+        }
+    }
+}
