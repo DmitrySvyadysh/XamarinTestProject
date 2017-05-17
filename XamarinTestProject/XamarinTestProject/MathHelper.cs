294e5d83e3353c2ce2cdaf73d4afce428866cd14
Dmitry Svyadysh <d.svyadysh@itransition.com>
2017-05-17 11:59:07 +0300

    Added unit tests

---
+++ XamarinTestProject/XamarinTestProject/MathHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XamarinTestProject
+{
+    public class MathHelper
+    {
+        public int Max(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+    }
+}
